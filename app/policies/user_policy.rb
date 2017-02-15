8474e6f0357b412850a373b36e86ff5b3a2e42bb
Adam <adam.lee346@gmail.com>
2017-02-15 07:52:36 +0000

    added pundit setup to application controller

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,17 @@
+class UserPolicy 
+    # always takes user and model 
+    attr_reader :current_user, :model
+    
+    # initialize 
+    def initialize(current_user, model)
+        @current_user = current_user
+        @user = model 
+    end 
+    
+    # is the current user an admin? 
+    # if so it can go to index and see index 
+    # else it is access denied
+    def index?  
+        @current_user.admin?    
+    end 
+end 
