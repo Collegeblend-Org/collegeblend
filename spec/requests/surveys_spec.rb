32152719248f007d3677e170c13807762663f69f
Derek Noble <dnoble11@msudenver.edu>
2017-04-28 02:55:15 +0000

    Included additional testing source and updates to scaffold controllers

--- spec/requests/surveys_spec.rb
+++ spec/requests/surveys_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Surveys", type: :request do
   describe "GET /surveys" do
-    it "works! (now write some real specs)" do
+    it "works!" do
       get surveys_path
       expect(response).to have_http_status(200)
     end
